2e15c61d4e5a28bb1ced1127932b88ae135fb785
Joel Macey <joel.macey@thedatafoundry.com.au>
2023-06-08 10:36:14 +1000

    formatting and linting

--- directory_search.py
+++ directory_search.py
@@ -1,11 +1,14 @@
 import argparse
 
+
 def main(directory):
     print(directory)
 
-if __name__ == "__main__":
 
+if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("--directory", type=str, default='.', help="the directory to search")
+    parser.add_argument(
+        "--directory", type=str, default=".", help="the directory to search"
+    )
     args = parser.parse_args()
     main(args.directory)
